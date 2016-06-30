f3e1bdd26e215bdd38b42f0c7007d35a0b17adb0
lpt <lpt42@hotmail.fr>
2016-06-30 21:09:54 +0200

    ajout au commit de Entity annonce

---
+++ src/GA/CoreBundle/Entity/Annonce.php
@@ -0,0 +1,261 @@
+<?php
+
+namespace GA\CoreBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Annonce
+ *
+ * @ORM\Table(name="annonce")
+ * @ORM\Entity(repositoryClass="GA\CoreBundle\Repository\AnnonceRepository")
+ */
+class Annonce
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="dateCreat", type="datetimetz")
+     */
+    private $dateCreat;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="dateModif", type="datetimetz", nullable=true)
+     */
+    private $dateModif;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="titre", type="string", length=255)
+     */
+    private $titre;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="auteur", type="string", length=255)
+     */
+    private $auteur;
+
+    /**
+     * @var text
+     *
+     * @ORM\Column(name="contenu", type="text")
+     */
+    private $contenu;
+		
+		/**
+		* @var int
+		*
+		* @ORM\Column(name="type", type="integer")
+		*/
+		private $type;
+		
+		/**
+     * @var string
+     *
+     * @ORM\Column(name="auteurModif", type="string", length=255)
+     */
+    private $auteurModif;
+		
+		
+		
+		public function __construct()
+		{
+				$this->dateCreat = new \DateTime();
+				$this->dateModif = new \DateTime();
+		}
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set datetime
+     *
+     * @param \DateTime $datetime
+     *
+     * @return Annonce
+     */
+    public function setDateCreat($dateCreat)
+    {
+        $this->datetime = $dateCreat;
+
+        return $this;
+    }
+
+    /**
+     * Get datetime
+     *
+     * @return \DateTime
+     */
+    public function getDateCreat()
+    {
+        return $this->dateCreat;
+    }
+
+    /**
+     * Set dateModif
+     *
+     * @param \DateTime $dateModif
+     *
+     * @return Annonce
+     */
+    public function setDateModif($dateModif)
+    {
+        $this->dateModif = $dateModif;
+
+        return $this;
+    }
+
+    /**
+     * Get dateModif
+     *
+     * @return \DateTime
+     */
+    public function getDateModif()
+    {
+        return $this->dateModif;
+    }
+
+    /**
+     * Set titre
+     *
+     * @param string $titre
+     *
+     * @return Annonce
+     */
+    public function setTitre($titre)
+    {
+        $this->titre = $titre;
+
+        return $this;
+    }
+
+    /**
+     * Get titre
+     *
+     * @return string
+     */
+    public function getTitre()
+    {
+        return $this->titre;
+    }
+
+    /**
+     * Set auteur
+     *
+     * @param string $auteur
+     *
+     * @return Annonce
+     */
+    public function setAuteur($auteur)
+    {
+        $this->auteur = $auteur;
+
+        return $this;
+    }
+
+    /**
+     * Get auteur
+     *
+     * @return string
+     */
+    public function getAuteur()
+    {
+        return $this->auteur;
+    }
+
+    /**
+     * Set contenu
+     *
+     * @param text $contenu
+     *
+     * @return Annonce
+     */
+    public function setContenu($contenu)
+    {
+        $this->contenu = $contenu;
+
+        return $this;
+    }
+
+    /**
+     * Get contenu
+     *
+     * @return text
+     */
+    public function getContenu()
+    {
+        return $this->contenu;
+    }
+		
+		
+
+    /**
+     * Set type
+     *
+     * @param integer $type
+     *
+     * @return Annonce
+     */
+    public function setType($type)
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    /**
+     * Get type
+     *
+     * @return integer
+     */
+    public function getType()
+    {
+        return $this->type;
+    }
+
+    /**
+     * Set auteurModif
+     *
+     * @param string $auteurModif
+     *
+     * @return Annonce
+     */
+    public function setAuteurModif($auteurModif)
+    {
+        $this->auteurModif = $auteurModif;
+
+        return $this;
+    }
+
+    /**
+     * Get auteurModif
+     *
+     * @return string
+     */
+    public function getAuteurModif()
+    {
+        return $this->auteurModif;
+    }
+}
