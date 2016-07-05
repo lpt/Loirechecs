90664d092540c217ddf6021d4f6a321cfa5329f7
lpt <lpt42@hotmail.fr>
2016-07-05 08:10:19 +0200

    test en dur many to one tournoi ronde

---
+++ src/GA/CoreBundle/Controller/TournoiController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace GA\CoreBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use GA\CoreBundle\Entity\Tournoi;
+use GA\CoreBundle\Entity\Ronde;
+
+class TournoiController extends Controller
+{
+    public function viewAction($id)
+    {
+			return $this->render('GACoreBundle:Tournoi:view.html.twig', array('id' => $id));
+    }
+		
+		public function addAction()
+		{	
+			$tournoi = new Tournoi();
+			$tournoi->setNom('Tournoi Test1');
+			$tournoi->setDescription('Description du tournoi Test1');
+			$tournoi->setContactNom('GA');
+			$tournoi->setContactTph('06-45-27-54-39');
+			$tournoi->setContactMail('gandre@etn.fr');
+			
+			$ronde1 = new Ronde();
+			$ronde1->setNumero(1);
+			$ronde1->setDateEvent(New \DateTime);
+			$ronde1->setAdresse('36 Rue Des Vercheres');
+			$ronde1->setVille('GENILAC');
+			
+			$ronde2 = new Ronde();
+			$ronde2->setNumero(2);
+			$ronde2->setDateEvent(New \DateTime);
+			$ronde2->setAdresse('36 Rue Des Vercheres');
+			$ronde2->setVille('GENILAC');
+			
+			$ronde1->setTournoi($tournoi);
+			$ronde2->setTournoi($tournoi);
+			
+			$em = $this->getDoctrine()->getManager();
+			$em->persist($tournoi);
+			$em->persist($ronde1);
+			$em->persist($ronde2);
+			$em->flush();
+			
+			return $this->redirectToRoute('ga_core_annonce', array('page' => 1));
+			//return $this->render('GACoreBundle:Tournoi:add.html.twig');
+			
+		}
+		
+		public function editAction($id)
+		{
+			
+			return $this->render('GACoreBundle:Tournoi:edit.html.twig', array('id' => $id));
+		}
+		
+		public function deleteAction($id)
+		{
+			
+			return $this->render('GACoreBundle:Tournoi:delete.html.twig', array('id' => $id));
+		}
+}
---
+++ src/GA/CoreBundle/Entity/Ronde.php
@@ -0,0 +1,189 @@
+<?php
+
+namespace GA\CoreBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Ronde
+ *
+ * @ORM\Table(name="ronde")
+ * @ORM\Entity(repositoryClass="GA\CoreBundle\Repository\RondeRepository")
+ */
+class Ronde
+{		
+
+		/**
+			* @ORM\ManyToOne(targetEntity="GA\CoreBundle\Entity\Tournoi")
+			* @ORM\JoinColumn(nullable=false)
+			*/
+		private $tournoi;
+	 
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="numero", type="integer")
+     */
+    private $numero;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="dateEvent", type="datetime")
+     */
+    private $dateEvent;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="adresse", type="string", length=255)
+     */
+    private $adresse;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="ville", type="string", length=255)
+     */
+    private $ville;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set numero
+     *
+     * @param integer $numero
+     *
+     * @return Ronde
+     */
+    public function setNumero($numero)
+    {
+        $this->numero = $numero;
+
+        return $this;
+    }
+
+    /**
+     * Get numero
+     *
+     * @return int
+     */
+    public function getNumero()
+    {
+        return $this->numero;
+    }
+
+    /**
+     * Set dateEvent
+     *
+     * @param \DateTime $dateEvent
+     *
+     * @return Ronde
+     */
+    public function setDateEvent($dateEvent)
+    {
+        $this->dateEvent = $dateEvent;
+
+        return $this;
+    }
+
+    /**
+     * Get dateEvent
+     *
+     * @return \DateTime
+     */
+    public function getDateEvent()
+    {
+        return $this->dateEvent;
+    }
+
+    /**
+     * Set adresse
+     *
+     * @param string $adresse
+     *
+     * @return Ronde
+     */
+    public function setAdresse($adresse)
+    {
+        $this->adresse = $adresse;
+
+        return $this;
+    }
+
+    /**
+     * Get adresse
+     *
+     * @return string
+     */
+    public function getAdresse()
+    {
+        return $this->adresse;
+    }
+
+    /**
+     * Set ville
+     *
+     * @param string $ville
+     *
+     * @return Ronde
+     */
+    public function setVille($ville)
+    {
+        $this->ville = $ville;
+
+        return $this;
+    }
+
+    /**
+     * Get ville
+     *
+     * @return string
+     */
+    public function getVille()
+    {
+        return $this->ville;
+    }
+
+    /**
+     * Set tournoi
+     *
+     * @param \GA\CoreBundle\Entity\Tournoi $tournoi
+     *
+     * @return Ronde
+     */
+    public function setTournoi(\GA\CoreBundle\Entity\Tournoi $tournoi)
+    {
+        $this->tournoi = $tournoi;
+
+        return $this;
+    }
+
+    /**
+     * Get tournoi
+     *
+     * @return \GA\CoreBundle\Entity\Tournoi
+     */
+    public function getTournoi()
+    {
+        return $this->tournoi;
+    }
+}
