34b5975cd9e9af67650c0aea72c0d81950892a8c
lpt <lpt42@hotmail.fr>
2016-08-04 19:22:08 +0200

    FOSUSerBundle + user Bundle + config mini

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,6 +16,8 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new GA\CoreBundle\GACoreBundle(),
+						new FOS\UserBundle\FOSUserBundle(),
+            new GA\UserBundle\GAUserBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
---
+++ src/GA/UserBundle/GAUserBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace GA\UserBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class GAUserBundle extends Bundle
+{
+}
